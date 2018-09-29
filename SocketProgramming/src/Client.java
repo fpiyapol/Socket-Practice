91a3eb77ca08480e3b1589dcaaa71f87d797eba9
fpiyapol <33046361+fpiyapol@users.noreply.github.com>
2018-09-29 21:58:53 +0700

    Client cont
    cont.

--- SocketProgramming/src/Client.java
+++ SocketProgramming/src/Client.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Client {
 	BufferedReader in;
@@ -14,6 +18,10 @@ public class Client {
         System.out.println(in.readLine());
         out = new PrintWriter(socket.getOutputStream(), true);
 		
+        while(true) {
+        	String line = in.readLine();
+        	System.out.println(line);
+        }
 	}
 	public static void main(String[] args) throws Exception {
 		Client c = new Client();
